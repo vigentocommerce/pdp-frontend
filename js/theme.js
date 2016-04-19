f43079dbf413c1ce6d500cfec8a90d29096cefe7
Calvin Cao <vigentocommerce@gmail.com>
2016-04-19 22:16:40 +0700

    End Apr 19

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function($) {
 	/* Top help dropdown */
 	 $('.toppage').on("click", ".help", function(){ 
-        $('.tooltip-outline').toggle();
+        $('.tooltip-outline').toggleClass('expanded');
     });
 	/* Side Switcher */
 	$('.toolbar').on("click",".side-down",function(){
@@ -13,5 +13,14 @@ jQuery(document).ready(function($) {
 	$('.toolbar').on("click",".action-down",function(){
 		$('.main-actions ul').toggleClass('expanded');
 	});
-	
+	/* Toggle Block left */
+	$('.toolbar').on("click",".nav-toggle",function(){
+		$(".lnr-menu, .lnr-cross").toggleClass("lnr-menu lnr-cross"); 
+		$('.block-sidebar-left').toggleClass('expanded');
+	});
+	/* Hidden all expanded class by click on main view */
+	$('.main-view').on('click',function(){
+		$('body').find('.expanded').removeClass('expanded');
+		
+	});
 });
